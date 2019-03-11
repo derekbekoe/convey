79f01ab96c7e76d8b2b62f2dfa1e5612f737d57b
Derek Bekoe <debekoe@microsoft.com>
2019-03-10 17:54:59 -0700

    Support TLS connections & update README (#12)
    Add `--demo` option to connect to demo server.
    Add `--unsecure` option for testing purposes.

--- README.md
+++ README.md
@@ -1,43 +1,79 @@
-# convey
+<p align="center">
+  <img 
+    src="https://derekb.blob.core.windows.net/public/convey_1.svg" 
+    width="400" border="0" alt="Convey">
+</p>
+<p align="center">
+<a href="https://github.com/derekbekoe/convey/releases"><img src="https://img.shields.io/github/release/derekbekoe/convey.svg?style=flat-square&logo=github&color=%236C63FF" alt="Version"></a>
+<a href="https://travis-ci.org/derekbekoe/convey"><img src="https://img.shields.io/travis/derekbekoe/convey/master.svg?style=flat-square&logo=travis" alt="Build Status"></a>
+</p>
+<div align="center">
+<p>A command-line tool that makes sharing pipes between machines easy.</p>
+<p>Learn more at <a href="https://blog.derekbekoe.com/convey"><em>Convey: Pipe between machines</em></a></p>
+</div>
 
-A command-line tool that makes sharing pipes between machines easy.
 
-## Install
-
-**Install on Linux:**
-```bash
-wget -qO convey https://get.convey.sh/linux
-chmod +x ~/bin/convey
-```
-
-**Install on Mac OS:**
-```bash
-wget -qO ~/bin/convey https://get.convey.sh/macos
-chmod +x ~/bin/convey
-```
-
-**Install on Windows:**  
-> Download from https://get.convey.sh/windows
-
-This will download the latest release for your platform.  
-Builds are available for the `amd64` architecture.  
-To directly from GitHub, visit https://github.com/derekbekoe/convey/releases/latest.  
-
-## Usage
-
-In Terminal 1:
 ```bash
 echo "Hello world" | convey
 21f50fba373e11e9990a72000872a940
 ```
-
-In Terminal 2:
 ```bash
 convey 21f50fba373e11e9990a72000872a940
 Hello world
 ```
 
-## Configuration
+# Features
+
+- Pipe between hosts with an idomatic interface using the standard `|` symbol.
+- Easily pipe files between hosts.
+- Does not require any open ports between your clients.
+- Supports colors through [ANSI escape codes](https://en.wikipedia.org/wiki/ANSI_escape_code#Colors).
+- Supports Linux, macOS and Windows.
+- No dependencies to install.
+- Powered by [NATS](https://nats.io/), a CNCF project.
+
+# Getting Started
+
+## Install
+
+### Linux
+```bash
+wget -qO convey https://get.convey.sh/linux
+chmod +x ~/bin/convey
+~/bin/convey -h
+```
+
+### macOS
+```bash
+wget -qO ~/bin/convey https://get.convey.sh/macos
+chmod +x ~/bin/convey
+~/bin/convey -h
+```
+
+### Windows  
+```powershell
+Invoke-WebRequest https://get.convey.sh/windows -OutFile convey.exe
+convey -h
+```
+
+## Demo Mode
+
+A demo mode is available using the `--demo` flag.
+
+```bash
+echo "Hello world" | convey --demo
+<ID>
+```
+```bash
+convey --demo <ID>
+Hello world
+```
+
+Demo mode uses the `demo.nats.io` server over a TLS connection with channels expiring after 30 minutes of creation or 10 minutes of inactivity.
+
+Use this mode for experimental and getting started purposes only.
+
+# Configuration
 
 Set configuration with the `convey configure` command.
 ```bash
@@ -52,44 +88,48 @@ NatsURL: nats://localhost:4223
 NatsClusterID: test-cluster
 ```
 
-Use the --config flag on the command line to change the config file if needed.
+Use the `--config` flag on the command line to change the config file used if needed.
 
-## Development
-```bash
-go get -u github.com/derekbekoe/convey
-cd $GOPATH/src/github.com/derekbekoe/convey
-go run main.go
-go build -o bin/convey
-```
+# NATS Streaming Server
 
-## Host your own NATS Streaming Server
+You can host your own [NATS Streaming Server](https://nats.io/documentation/streaming/nats-streaming-intro/) and configure `convey` to use that server.
 
-**Deploy to a local Docker container**
+#### Deploy to a local Docker container
 
 ```bash
 docker run -p 4222:4222 nats-streaming:linux
 convey configure --nats-url nats://localhost:4222 --nats-cluster test-cluster
 ```
 
-**Deploy to an Azure Container Instance**
+You will need to use the `--unsecure` flag as TLS will not be enabled through this local container.
 
-note: We only include this as an illustration to keep the command simple as traffic is not encrypted.
+# Development
+
+**Set up**
 ```bash
-az container create --image nats-streaming:linux --ports 4222 --ip-address Public -g RG -n nats1
-convey configure --nats-url nats://<IPADDRESS>:4222 --nats-cluster test-cluster
+go get -u github.com/derekbekoe/convey
+cd $GOPATH/src/github.com/derekbekoe/convey
+go run main.go
 ```
 
-## Platform Builds
+**Multi-platform Builds**
 ```bash
 go get github.com/mitchellh/gox
 gox -ldflags "-X github.com/derekbekoe/convey/cmd.VersionGitCommit=$(git rev-list -1 HEAD) -X github.com/derekbekoe/convey/cmd.VersionGitTag=VERSION" -os="linux darwin" -arch="amd64" -output="bin/{{.Dir}}_{{.OS}}_{{.Arch}}"
 ```
 See https://golang.org/doc/install/source#environment
 
-## FAQ
+# Examples
 
-**How do I try it out?**
+Click to expand each gif.
 
-Start the local container, download convey, specify the configuration then run.
+<div style="text-align: center">
+<img src="https://derekb.blob.core.windows.net/public/blog_convey_vm_1_top.gif" alt="Convey with Top" border="0" width="445">
+<img src="https://derekb.blob.core.windows.net/public/blog_convey_vm_1_filecp.gif" alt="Convey for file piping" border="0" width="445">
+<img src="https://derekb.blob.core.windows.net/public/blog_convey_vm_1_ms.gif" alt="Convey with millisecond date" border="0" width="445">
+<img src="https://derekb.blob.core.windows.net/public/blog_convey_cloudshell_1.gif" alt="Convey with Cloud Shell" border="0" width="445">
+</div>
 
-If you'd like to share between multiple devices, host the server in a location where your devices can access.
+# License
+
+Convey source code is available under the [MIT License](LICENSE).
--- cmd/root.go
+++ cmd/root.go
@@ -28,36 +28,39 @@ import (
 
 	"github.com/docker/docker/pkg/namesgenerator"
 	homedir "github.com/mitchellh/go-homedir"
+	nats "github.com/nats-io/go-nats"
 	stan "github.com/nats-io/go-nats-streaming"
 	uuid "github.com/satori/go.uuid"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+const (
+	configKeyNatsURL       = "NatsURL"
+	configKeyNatsClusterID = "NatsClusterID"
+	configKeyUseShortName  = "UseShortName"
+	demoNatsURL            = "tls://demo.nats.io:4443"
+	demoNatsClusterID      = "convey-demo-cluster"
+)
+
+// Path to config file set by user
 var cfgFile string
+
+// Whether to log verbose output
 var verbose bool
 
-const configKeyNatsURL = "NatsURL"
-const configKeyNatsClusterID = "NatsClusterID"
-const configKeyUseShortName = "UseShortName"
+// Whether non-tls connection should be used for NATS connection
+var useUnsecure bool
 
-// ETX is End Of Text Sequence
-var ETX = []byte{3}
+// Whether the demo server and mode should be used
+var useDemoMode bool
+
+// etx is an identifier for End Of Text Sequence
+var etx = []byte{3}
 
 func errorExit(msg string) {
 	fmt.Fprintln(os.Stderr, msg)
 	os.Exit(1)
-}
-
-// positionalArgsValidator valids the positional args
-func positionalArgsValidator(cmd *cobra.Command, args []string) error {
-	if len(args) == 0 {
-		return nil
-	} else if len(args) == 1 {
-		_, err := uuid.FromString(args[0])
-		return err
-	}
-	return errors.New("Invalid positional arguments")
 }
 
 // rootCmd represents the base command when called without any subcommands
@@ -72,131 +75,12 @@ var rootCmd = &cobra.Command{
 // RootCommandFunc is a handler for the bare application
 func RootCommandFunc(cmd *cobra.Command, args []string) {
 	if len(args) == 0 {
-		PublishModeFunc()
+		publishModeFunc()
 	} else if len(args) == 1 {
-		SubscribeModeFunc(args[0])
+		subscribeModeFunc(args[0])
 	} else {
 		errorExit("Too many args")
 	}
-}
-
-func createChannelNameUUID() string {
-	u1, err := uuid.NewV1()
-	if err != nil {
-		s := fmt.Sprintf("Failed to create channel name: %s\n", err)
-		errorExit(s)
-	}
-	// Remove dashes from UUID to make copy-paste easier in terminal
-	return strings.Replace(u1.String(), "-", "", -1)
-}
-
-func createChannelNameShort() string {
-	return namesgenerator.GetRandomName(0)
-}
-
-func getClientID(prefix string) string {
-	u1, err := uuid.NewV1()
-	if err != nil {
-		s := fmt.Sprintf("Failed to create client ID: %s\n", err)
-		errorExit(s)
-	}
-	return fmt.Sprintf("%s-%s", prefix, u1.String())
-}
-
-func connectToStan(clientID string) stan.Conn {
-
-	natsURL := viper.GetString(configKeyNatsURL)
-	natsClusterID := viper.GetString(configKeyNatsClusterID)
-
-	if natsURL == "" || natsClusterID == "" {
-		s := fmt.Sprintf("The configuration options '%s' and '%s' are not set. Use `convey configure` to set.",
-			configKeyNatsURL,
-			configKeyNatsClusterID)
-		errorExit(s)
-	}
-
-	sc, err := stan.Connect(
-		natsClusterID,
-		clientID,
-		stan.NatsURL(natsURL),
-		stan.SetConnectionLostHandler(func(_ stan.Conn, err error) {
-			log.Printf("Lost connection due to error - %s", err)
-		}))
-	if err != nil {
-		s := fmt.Sprintf("Failed to connect to streaming server due to error - %s", err)
-		errorExit(s)
-	}
-	return sc
-}
-
-// PublishModeFunc handles publishing messages to message service
-func PublishModeFunc() {
-	clientID := getClientID("convey-pub")
-	sc := connectToStan(clientID)
-
-	useShortName := viper.GetBool(configKeyUseShortName)
-	channelName := ""
-	if useShortName {
-		channelName = createChannelNameShort()
-	} else {
-		channelName = createChannelNameUUID()
-	}
-
-	// Print channel to console for user to copy
-	fmt.Println(channelName)
-	log.Printf("Publishing to channel %s\n", channelName)
-
-	donePublish := make(chan bool)
-
-	// Handle Ctrl+C
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		donePublish <- true
-	}()
-
-	go func() {
-		scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
-		for scanner.Scan() {
-			line := scanner.Text()
-			sc.Publish(channelName, []byte(line))
-		}
-		donePublish <- true
-	}()
-
-	<-donePublish
-
-	sc.Publish(channelName, ETX)
-	sc.Close()
-}
-
-// SubscribeModeFunc handles reading messages from the message service
-func SubscribeModeFunc(channelName string) {
-	clientID := getClientID("convey-sub")
-	sc := connectToStan(clientID)
-
-	log.Printf("Subscribing to channel %s\n", channelName)
-
-	doneSubscribe := make(chan bool)
-
-	sub, subErr := sc.Subscribe(channelName, func(m *stan.Msg) {
-		if reflect.DeepEqual(m.Data, ETX) {
-			doneSubscribe <- true
-		} else {
-			fmt.Println(string(m.Data))
-		}
-	}, stan.DeliverAllAvailable())
-
-	if subErr != nil {
-		s := fmt.Sprintf("Failed to subscribe to channel %s due to error %s", channelName, subErr)
-		errorExit(s)
-	}
-
-	<-doneSubscribe
-
-	sub.Unsubscribe()
-	sc.Close()
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -214,8 +98,9 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default is $HOME/.convey.yaml)")
-
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().BoolVar(&useUnsecure, "unsecure", false, "use unsecured connection (for development purposes only)")
+	rootCmd.PersistentFlags().BoolVar(&useDemoMode, "demo", false, "use demo mode")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -245,4 +130,162 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err == nil {
 		log.Println("Config file:", viper.ConfigFileUsed())
 	}
+}
+
+// positionalArgsValidator valids the positional args
+func positionalArgsValidator(cmd *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return nil
+	} else if len(args) == 1 {
+		_, err := uuid.FromString(args[0])
+		return err
+	}
+	return errors.New("Invalid positional arguments")
+}
+
+func createChannelNameUUID() string {
+	u, err := uuid.NewV4()
+	if err != nil {
+		s := fmt.Sprintf("Failed to create channel name: %s\n", err)
+		errorExit(s)
+	}
+	// Remove dashes from UUID to make copy-paste easier in terminal
+	return strings.Replace(u.String(), "-", "", -1)
+}
+
+func createChannelNameShort() string {
+	return namesgenerator.GetRandomName(0)
+}
+
+func getClientID(prefix string) string {
+	u, err := uuid.NewV4()
+	if err != nil {
+		s := fmt.Sprintf("Failed to create client ID: %s\n", err)
+		errorExit(s)
+	}
+	return fmt.Sprintf("%s-%s", prefix, u.String())
+}
+
+func connectToStan(clientID string) (stan.Conn, *nats.Conn) {
+
+	natsURL := viper.GetString(configKeyNatsURL)
+	natsClusterID := viper.GetString(configKeyNatsClusterID)
+
+	if useDemoMode {
+		log.Printf("Using demo mode")
+		natsURL = demoNatsURL
+		natsClusterID = demoNatsClusterID
+	}
+
+	if natsURL == "" || natsClusterID == "" {
+		s := fmt.Sprintf("The configuration options '%s' and '%s' are not set. Use `convey configure` to set. Use `--help` for usage.",
+			configKeyNatsURL,
+			configKeyNatsClusterID)
+		errorExit(s)
+	}
+
+	natsSecureOpt := nats.Secure()
+
+	if useUnsecure {
+		log.Printf("Using unsecure connection to server")
+		natsSecureOpt = nil
+	}
+
+	natsConn, err1 := nats.Connect(natsURL, natsSecureOpt)
+	if err1 != nil {
+		s := fmt.Sprintf("Failed to connect to NATS server due to error - %s", err1)
+		errorExit(s)
+	}
+
+	stanConn, err := stan.Connect(
+		natsClusterID,
+		clientID,
+		stan.NatsConn(natsConn),
+		stan.SetConnectionLostHandler(func(_ stan.Conn, err error) {
+			log.Printf("Lost connection due to error - %s", err)
+		}))
+	if err != nil {
+		s := fmt.Sprintf("Failed to connect to streaming server due to error - %s", err)
+		errorExit(s)
+	}
+
+	return stanConn, natsConn
+}
+
+// publishModeFunc handles publishing messages to message service
+func publishModeFunc() {
+	clientID := getClientID("convey-pub")
+	stanConn, natsConn := connectToStan(clientID)
+
+	useShortName := viper.GetBool(configKeyUseShortName)
+	channelName := ""
+
+	if useDemoMode && useShortName {
+		log.Printf("Short names not allowed in demo mode to prevent possible conflicts")
+	}
+
+	// Check if should use short names. Short names not allowed in demo mode to prevent possible conflicts.
+	if useShortName && !useDemoMode {
+		channelName = createChannelNameShort()
+	} else {
+		channelName = createChannelNameUUID()
+	}
+
+	// Print channel to console for user to copy
+	fmt.Println(channelName)
+	log.Printf("Publishing to channel %s\n", channelName)
+
+	donePublish := make(chan bool)
+
+	// Handle Ctrl+C
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		donePublish <- true
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+		for scanner.Scan() {
+			line := scanner.Text()
+			stanConn.Publish(channelName, []byte(line))
+		}
+		donePublish <- true
+	}()
+
+	<-donePublish
+
+	stanConn.Publish(channelName, etx)
+	stanConn.Close()
+	natsConn.Close()
+}
+
+// subscribeModeFunc handles reading messages from the message service
+func subscribeModeFunc(channelName string) {
+	clientID := getClientID("convey-sub")
+	stanConn, natsConn := connectToStan(clientID)
+
+	log.Printf("Subscribing to channel %s\n", channelName)
+
+	doneSubscribe := make(chan bool)
+
+	sub, subErr := stanConn.Subscribe(channelName, func(m *stan.Msg) {
+		if reflect.DeepEqual(m.Data, etx) {
+			doneSubscribe <- true
+		} else {
+			fmt.Println(string(m.Data))
+		}
+	}, stan.DeliverAllAvailable())
+
+	if subErr != nil {
+		s := fmt.Sprintf("Failed to subscribe to channel %s due to error %s", channelName, subErr)
+		errorExit(s)
+	}
+
+	<-doneSubscribe
+
+	sub.Unsubscribe()
+	stanConn.Close()
+	natsConn.Close()
 }
